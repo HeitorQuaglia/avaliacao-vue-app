7fae069dc8fa6cad0a922bffd398ef7b12b133b5
Heitor Quaglia <heitor@venturelabs.gruop>
2021-10-14 21:36:33 -0300

    Refactoring Vue Home Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <b-row>
+      <b-col sm="3">
+        <label>Digite o código de rastreio ou seu CPF</label>
+      </b-col>
+      <b-col sm="7">
+        <b-input v-model="search"/>
+      </b-col>
+      <b-col sm="2">
+        <b-button variant="success" :active="active">Pesquisar</b-button>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-card :title="result.description">
+          <b-card-text>
+            Histórico
+            <b-list-group>
+              <b-list-group-item v-for="item in result.history" :key="item">
+                {{item}}
+              </b-list-group-item>
+            </b-list-group>
+          </b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    search: "",
+    active: true,
+    result: {
+      description: "#XPTO",
+      history: [
+        "XPTO"
+      ]
+    }
+  }),
+  methods: {
+
   }
 }
 </script>
