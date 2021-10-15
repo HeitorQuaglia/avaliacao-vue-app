c7cfc309a9db5085c16310f5b01f8ff43889e332
Heitor Quaglia <heitor@venturelabs.gruop>
2021-10-15 00:31:14 -0300

    remove unused plugins

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import axios from "axios";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
@@ -17,6 +16,5 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
